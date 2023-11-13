280fc082cc56f8e618077f2bada5494ac61c26ab
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-13 12:03:08 -0300

    implementado pokedex

---
+++ src/app/service/poke-api.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeApiService {
+
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get apiListAllPokemons():Observable<any>{
+    return this.http.get<any>(this.url).pipe(
+      tap( res => res ),
+      tap( res => {
+        res.results.map( (resPokemons: any) => {
+
+          this.apiGetPokemon(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          );
+
+          // this.http.get<any>( resPokemons.url ).pipe(
+          //   map(
+          //     res => res
+          //   )
+          // ).subscribe(
+          //   res => resPokemons.status = res
+          // )
+
+        })
+      })
+    )
+  }
+
+  public apiGetPokemon( url: string ):Observable<any>{
+    return this.http.get<any>( url ).pipe(
+      map(
+        res => res
+      )
+    )
+  }
+}
